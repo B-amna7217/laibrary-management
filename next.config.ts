9d9480e9346ebe9b18d352beae94ebf3a7613cc3
Bibi amna <amnab7217@gmail.com>
2025-08-27 14:33:43 -0400

    fgggg

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: false, // disable LightningCSS
-  },
-  compiler: {
-    removeConsole: false, // optional
-  },
+  /* config options here */
 };
 
 export default nextConfig;
